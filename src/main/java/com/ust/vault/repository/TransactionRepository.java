15a16c7d84da8fcdf7871bea4cf47326409e31c7
NaveenKotapati1436 <naveenkotapati1436@gmail.com>
2024-09-26 19:00:42 +0530

    vault-front

--- src/main/java/com/ust/vault/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ust.vault.repository;
-
-import com.ust.vault.model.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
